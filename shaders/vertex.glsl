deef5e63e5efaca7fa370d08583a4e1b196224fd
Heritiana Rafidimanantsoa Lhario <rafidimanantsoaheritiana@gmail.com>
2024-09-27 23:58:11 +0300

    maj

---
+++ shaders/vertex.glsl
@@ -0,0 +1,7 @@
+varying vec2 uv;
+void main() {
+  vUv = uv;
+  vec4 mvPosition = modelViewMatrix * vec4(position, 1.);
+  gl_PointSize = 50. * (1. / - mvPosition.z);
+  gl_Position = projectionMatrix * mvPosition;
+}
