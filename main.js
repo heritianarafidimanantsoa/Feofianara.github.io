ae29604836b2bac046a3d8a15c4013e22c129df7
Mecdaddy09 <132587031+Mecdaddy09@users.noreply.github.com>
2024-10-22 17:57:59 +0200

    resoudre json3

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ import gsap from "gsap";
 
 // import locationsData from "./data.json" assert { type: "json" };
 
-import data from "./360.json" assert { type: "json" };
+import data from "./360.json" with { type: "json" };
 import { convertSpeed } from "geolib";
 
 // Récupérer la modale
